18b4ee279a60d6eb310abd0bdc19b046f4a1743a
nwang15 <nawang1019@gmail.com>
2022-05-12 18:09:30 -0600

    update

--- src/test/java/com/test/ctomsAutoTest.java
+++ src/test/java/com/test/ctomsAutoTest.java
@@ -1,6 +1,7 @@
 package com.test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.ClassOrderer;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -13,6 +14,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
@@ -24,15 +28,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 import io.github.bonigarcia.seljup.SingleSession;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
 
 @ExtendWith(SeleniumJupiter.class)
-@TestMethodOrder(OrderAnnotation.class)
+//@TestMethodOrder(OrderAnnotation.class)
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
 @SingleSession
+@TestInstance(Lifecycle.PER_CLASS)
 class ctomsAutoTest {
 
     WebDriver driver;
@@ -42,14 +50,11 @@ class ctomsAutoTest {
     }
     @Test
     @Order(1)
-    void init() {
+    void init() throws MalformedURLException, IOException, InterruptedException {
         
         driver.get("https://www.ctomsinc.com/");
         driver.manage().window().maximize();
-    }
-    @Test
-    void getStatusCode () throws MalformedURLException, IOException
-    {
+    
     	 HttpURLConnection cont=
 			      (HttpURLConnection)new URL("https://www.ctomsinc.com/")
 			      .openConnection();
@@ -59,15 +64,17 @@ class ctomsAutoTest {
 	      cont.connect();
 	      int rs = cont.getResponseCode();
 	      System.out.println("Http response code: " + rs);	
+	      Thread.sleep(3000);
     }
+    
+   
   
     @Nested
     @Order(2)
     @DisplayName("Check Home Page Info")
-    class homePageInfo {
+    class ahomePageInfo {
  
     	@Test 	
-    	@Order(2)
         void getMeta() {
             
         	 String content = driver.findElement(By.xpath("//meta[@name='description']"))
@@ -75,7 +82,7 @@ class ctomsAutoTest {
     		      System.out.println("Meta Description Content: " + content);	      
         }
     	@Test  
-    	@Order(2)
+    	
          void CheckImage() throws Exception {
         	
         	WebElement ImageFile = driver.findElement(By.xpath("//img[@src='//cdn.shopify.com/s/files/1/0111/5966/6788/files/FireflySlideshowV2_1800x1000_crop_center.png?v=1647456343']"));
@@ -90,45 +97,46 @@ class ctomsAutoTest {
                     System.out.println("Image displayed.");
                 }
         	} 
-    }
-
-    @Test
-    @Order(3)
-    @DisplayName("Click Shop Now Button")
-    void ShopNow() throws InterruptedException {
-        
-        WebElement button = driver.findElement(By.linkText("SHOP NOW"));
-        assertTrue(button.isDisplayed());
-        button.click();
-        Thread.sleep(3000);
-        //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-    	}  
+    	}
     
-    @Nested
-    @Order(4)
-    @DisplayName("Check product page info")
-    class productInfo {
- 
-     
-      @Test
-      @DisplayName("Click Product by Product Title")
-      void clickProduct() throws InterruptedException {
-          
-          WebElement firflyProduct = driver.findElement(By.linkText("Paraglider Kit, FireFly Kit"));
-          assertTrue(firflyProduct.isDisplayed());
-          firflyProduct.click();
-          Thread.sleep(5000);
-          //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-      	}   
-      @Test
-      @DisplayName("Click Product Title if it displays")
-      void clickProductTitle() throws InterruptedException {
-    	  
-    	// check product title element if displayed in page
-	      Boolean Display = driver.findElement(By.xpath("//*[@class='product-title']")).isDisplayed();
-	      System.out.println("Title element displayed is:" + Display);
-          Thread.sleep(5000);
-          //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-      	}  
-    }
+
+
+	    @Nested
+	    @Order(4)
+	    @DisplayName("Check product page info")
+	    class cproductInfo {	
+	    	 @Test
+	       	 void ShopNow() throws InterruptedException  {      
+	    	        WebElement button = driver.findElement(By.xpath("//a[@href='https://ctomsinc.com/collections/high-angle-aviation-equipment/high-angle-aviation-equipment-kits_firefly-kits']"));
+	    	        assertTrue(button.isDisplayed());
+	    	        button.click();
+	    	        Thread.sleep(3000);
+	    	 
+	    }
+	      @Test
+	      @DisplayName("Click Product by Product Title")
+	      void clickProduct() throws InterruptedException {
+	          
+	          WebElement firflyProduct = driver.findElement(By.linkText("Paraglider Kit, FireFly Kit"));
+	          assertTrue(firflyProduct.isDisplayed());
+	          firflyProduct.click();
+	          Thread.sleep(5000);
+	          //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+	      	} 
+	      
+	      @Test
+	      @DisplayName("Check Product Title if it displays")
+	      void zcheckProductTitle() throws InterruptedException {
+	    	  
+	    	// check product title element if displayed in page
+	 	      Boolean Display = driver.findElement(By.xpath("//*[@class='product-title']")).isDisplayed();
+	 	      System.out.println("Title element displayed is:" + Display);
+	          Thread.sleep(5000);
+	          //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+	      	}  
+	}
+
+    
+    
+
 }
